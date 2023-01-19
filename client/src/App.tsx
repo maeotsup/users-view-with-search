c65ed9c903e5b6dbd951bc4919710138f5bdffc4
Peeter Maeots <peeter.maeots@uptime.eu>
2023-01-19 16:48:26 +0200

    moved client code to client folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { User } from './types/User';
+
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {!!users.length && users.map(user => (<p key={user.id}>{user.name}</p>))}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
