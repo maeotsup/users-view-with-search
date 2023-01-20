6f274969c752811a74c0a1c125473a1c20eb2678
Peeter Mäeots <72010952+maeotsup@users.noreply.github.com>
2023-01-20 18:47:11 +0200

    Feature/redux toolkit (#5)
    * added redux-toolkit and react-redux libraries
    
    * moved css to styles folder
    
    * created redux store for users
    
    * use redux toolkit and store for users table
    
    * revert semantic ui css changes
    
    * code cleanup
    
    * removed unused reference
    
    * show users table when users fetched

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,10 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import './styles/root.css';
 
 import UsersTable from './components/UsersTable';
-import { IUser } from '../../server/src/interfaces/user.interfaces';
 
 function App() {
-  const [users, setUsers] = useState<IUser[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  useEffect(() => {
-    setLoading(true);
-    fetch('/api/users')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setUsers(data);
-        setLoading(false);
-      });
-  }, []);
   return (
-    <div id="App">
-      <UsersTable loading={loading} users={users} />
-    </div>
+    <UsersTable />
   );
 }
 
