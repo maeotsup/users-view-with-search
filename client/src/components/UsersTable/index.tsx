c710473c301d480b1ee624b6a7536b9740259722
Peeter Maeots <peeter.maeots@uptime.eu>
2023-01-20 18:40:36 +0200

    use redux toolkit and store for users table

--- client/src/components/UsersTable/index.tsx
+++ client/src/components/UsersTable/index.tsx
@@ -1,23 +1,36 @@
-import { Table } from 'semantic-ui-react';
+import { useEffect } from 'react';
+import { Message, Table } from 'semantic-ui-react';
 
-import { IUser } from '../../../../server/src/interfaces/user.interfaces';
+import { useAppSelector, useAppDispatch } from '../../hooks/store';
+import { fetchAllUsersAsync } from '../../store/slices/users';
 import LoaderCentered from '../LoaderCentered';
 import TableHeader from './TableHeader';
 import TableBody from './TableBody';
 
-interface IUsersTableProps {
-  loading: boolean;
-  users?: IUser[];
-}
+const UsersTable = () => {
+  const dispatch = useAppDispatch();
+  const { loading, users } = useAppSelector(store => store.users);
+  
+  useEffect(() => {
+    const getUsersAsync = async () => await dispatch(fetchAllUsersAsync());
+    getUsersAsync();
+  }, [dispatch]);
 
-const UsersTable = ({ loading, users }: IUsersTableProps) => {
-  if (loading) return <LoaderCentered>Loading users...</LoaderCentered>
-  return !users ? null : (
-    <Table celled padded='very' size='large' striped>
-      <TableHeader />
-      <TableBody users={users} />
-    </Table>
-  );
+  if (loading) return <LoaderCentered>Loading users...</LoaderCentered>;
+
+  return users
+    ? (
+      <Message
+        header='Oops! No users found.'
+        className='flex-justify-content-center'
+        warning
+      />
+    ) : (
+      <Table celled padded='very' size='large' striped>
+        <TableHeader />
+        <TableBody users={users} />
+      </Table>
+    );
 };
 
 export default UsersTable;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import { store } from './store';
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-// Add Seamntic Ui React stylesheet
-const styleLink = document.createElement('link');
-styleLink.rel = 'stylesheet';
-styleLink.href = 'https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css';
-document.head.appendChild(styleLink);
-
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
