da70059fcc283c69de5cf45c0ad0ca814687272d
Peeter Maeots <peeter.maeots@uptime.eu>
2023-01-20 20:08:49 +0200

    use semantic ui css library

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import 'semantic-ui-css/semantic.min.css';
 
 import { store } from './store';
 import App from './App';
@@ -8,12 +9,6 @@ import App from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-// Add Seamntic Ui React stylesheet
-const styleLink = document.createElement('link');
-styleLink.rel = 'stylesheet';
-styleLink.href = 'https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css';
-document.head.appendChild(styleLink);
 
 root.render(
   <React.StrictMode>
