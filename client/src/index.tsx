bd5f8272f7c4da3758effac4fec8b0267802ab53
Peeter Maeots <peeter.maeots@uptime.eu>
2023-01-20 18:45:16 +0200

    revert semantic ui css changes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -9,6 +9,12 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+// Add Seamntic Ui React stylesheet
+const styleLink = document.createElement('link');
+styleLink.rel = 'stylesheet';
+styleLink.href = 'https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css';
+document.head.appendChild(styleLink);
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
