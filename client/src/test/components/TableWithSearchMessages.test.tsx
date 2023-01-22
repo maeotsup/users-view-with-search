02d9920f1e9dfe2771514e3dfdc2b5af61098951
Peeter Maeots <peeter.maeots@uptime.eu>
2023-01-22 14:44:40 +0200

    test for table with results

--- client/src/test/components/TableWithSearchMessages.test.tsx
+++ client/src/test/components/TableWithSearchMessages.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 
 import TableWithSearchMessages from '../../components/UsersTable/TableWithSearchMessages';
+import { IUser } from '../../../../server/src/interfaces/user.interfaces';
 
 test('TableWithSearchMessages component shows loader when users search is in progress', () => {
   const props = {
@@ -37,6 +38,34 @@ test('TableWithSearchMessages shows no results message when users search has no 
 
   const getErrorMessage = await screen.findByTestId('no-search-results');
   expect(getErrorMessage).toBeDefined();
+});
+
+test('TableWithSearchMessages shows users table with results', () => {
+  const props = {
+    noSearchResults: false,
+    searching: false,
+    searchResults: [{
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      website: 'hildegard.org'
+    }] as IUser[],
+  };
+
+  render(
+    <TableWithSearchMessages
+      noSearchResults={props.noSearchResults}
+      searching={props.searching}
+      searchResults={props.searchResults}
+    />
+  );
+
+  expect(screen.getByRole('table')).toBeInTheDocument();
+  expect(screen.getByText(/Leanne Graham/i)).toBeDefined();
+  expect(screen.getByText(/Bret/i)).toBeDefined();
+  expect(screen.getByText(/Sincere@april.biz/i)).toBeDefined();
+  expect(screen.getByText(/hildegard.org/i)).toBeDefined();
 });
 
 export {};
